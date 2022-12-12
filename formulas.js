c55273eaf9c28ae2fcbc0533bceda0ebc4a3533b
lobatov <qlobatov@gmail.com>
2022-12-11 23:28:06 -0600

    Creacion de archivo CSS y JS

---
+++ formulas.js
@@ -0,0 +1,50 @@
+function formula() {
+    var CantidadT = parseInt(document.getElementById('Cant').value);
+    var Cantidad1 = parseInt(document.getElementById('C1').value);
+    var Cantidad2 = parseInt(document.getElementById('C2').value);
+
+    document.getElementById('C1').value = (Cantidad1 - CantidadT);
+    document.getElementById('C2').value = (Cantidad2 + CantidadT);
+}
+
+
+function formula2() {
+    var CantidadT = parseInt(document.getElementById('Cant').value);
+    var Cantidad1 = parseInt(document.getElementById('C1').value);
+    var Cantidad2 = parseInt(document.getElementById('C2').value);
+
+    document.getElementById('C2').value = (Cantidad2 - CantidadT);
+    document.getElementById('C1').value = (Cantidad1 + CantidadT);
+}
+
+        
+function formula3() {
+    var CantidadT = parseInt(document.getElementById('Cant').value);
+    var Cantidad1 = parseInt(document.getElementById('C1').value);
+
+        document.getElementById('C1').value = (Cantidad1 + CantidadT);
+    }
+
+
+function formula4() {
+    var CantidadT = parseInt(document.getElementById('Cant').value);
+    var Cantidad2 = parseInt(document.getElementById('C2').value);
+
+        document.getElementById('C2').value = (Cantidad2 + CantidadT);
+}
+
+
+function formula5() {
+    var CantidadT = parseInt(document.getElementById('Cant').value);
+    var Cantidad1 = parseInt(document.getElementById('C1').value);
+
+    document.getElementById('C1').value = (Cantidad1 - CantidadT);
+}
+
+
+function formula6() {
+    var CantidadT = parseInt(document.getElementById('Cant').value);
+    var Cantidad2 = parseInt(document.getElementById('C2').value);
+
+    document.getElementById('C2').value = (Cantidad2 - CantidadT);
+}
